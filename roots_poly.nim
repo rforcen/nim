183ddde79a5c51aa6cbe651cc6e0ef68ac3d8003
roberto forcen <robertoforcen@gmail.com>
2021-10-11 13:18:18 +0200

    Add files via upload

--- roots_poly.nim
+++ roots_poly.nim
@@ -2,61 +2,62 @@
 
 import complex, algorithm, logging
 
-proc laguer(a: var seq[Complex64], x: var Complex64, its: var int) =
-  const
-    MR = 8
-    MT = 10
-    MAXIT = MT * MR
-    EPS = 2.22045e-16 # numeric_limits<Doub>::epsilon()
-    frac = [0.0, 0.5, 0.25, 0.75, 0.13, 0.38, 0.62, 0.88, 1.0]
-
-  let m = a.len - 1
-
-  for iter in 1..MAXIT:
-    its = iter
-
-    var 
-      b = a[m]
-      err = b.abs
-      d, f : Complex64
-      abx = x.abs
-
-    for j in countdown(m - 1, 0):
-      f = x * f + d
-      d = x * d + b
-      b = x * b + a[j]
-      err = b.abs + abx * err
-
-    err *= EPS
-    if b.abs <= err: return
-
-    var 
-      g = d / b
-      g2 = g * g
-      h = g2 - 2.0 * f / b
-      sq = ((m - 1).float * (m.float * h - g2)).sqrt
-      gp = g + sq
-      gm = g - sq
-
-      abp = gp.abs
-      abm = gm.abs
-
-    if abp < abm: gp = gm
-
-    var dx = if max(abp, abm) > 0.0: m.float / gp
-    else:
-      let p = complex64(1.0 + abx, iter.float).polar
-      complex64(p.r, p.phi)
-
-    var x1 = x - dx
-    if x == x1: return
-
-    if iter %% MT != 0: x = x1
-    else: x -= frac[iter div MT] * dx
-
-  error "too many iterations in laguer"
 
 proc zroots*(a, roots: var seq[Complex64], polish: bool) =
+  proc laguer(a: var seq[Complex64], x: var Complex64, its: var int) =
+    const
+      MR = 8
+      MT = 10
+      MAXIT = MT * MR
+      EPS = 2.22045e-16 # numeric_limits<Doub>::epsilon()
+      frac = [0.0, 0.5, 0.25, 0.75, 0.13, 0.38, 0.62, 0.88, 1.0]
+
+    let m = a.len - 1
+
+    for iter in 1..MAXIT:
+      its = iter
+
+      var 
+        b = a[m]
+        err = b.abs
+        d, f : Complex64
+        abx = x.abs
+
+      for j in countdown(m - 1, 0):
+        f = x * f + d
+        d = x * d + b
+        b = x * b + a[j]
+        err = b.abs + abx * err
+
+      err *= EPS
+      if b.abs <= err: return
+
+      var 
+        g = d / b
+        g2 = g * g
+        h = g2 - 2.0 * f / b
+        sq = ((m - 1).float * (m.float * h - g2)).sqrt
+        gp = g + sq
+        gm = g - sq
+
+        abp = gp.abs
+        abm = gm.abs
+
+      if abp < abm: gp = gm
+
+      var dx = if max(abp, abm) > 0.0: m.float / gp
+      else:
+        let p = complex64(1.0 + abx, iter.float).polar
+        complex64(p.r, p.phi)
+
+      var x1 = x - dx
+      if x == x1: return
+
+      if iter %% MT != 0: x = x1
+      else: x -= frac[iter div MT] * dx
+
+    error "too many iterations in laguer"
+
 
   const EPS = 1.0e-14
   var
@@ -87,6 +88,7 @@ proc zroots*(a, roots: var seq[Complex64], polish: bool) =
       laguer(a, roots[j], its)
 
   roots.sort(proc (x, y: Complex64): int = (if x.re < y.re: -1 else: 1))
+
 
 
 when isMainModule:
