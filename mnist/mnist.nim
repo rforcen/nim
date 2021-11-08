679704de21a6f2d3faddf3741f8e9abe7fbc3bd4
roberto forcen <robertoforcen@gmail.com>
2021-11-08 09:51:28 +0100

    Add files via upload

---
+++ mnist/mnist.nim
@@ -0,0 +1,164 @@
+# mnist reader
+
+import streams, endians, zip/zipfiles, sequtils, random
+import mat
+
+{.experimental: "parallel".}
+
+const 
+  path="data/"
+  zip_file = "mnist/mnist.zip"
+
+  tr_img="train-images-idx3-ubyte"
+  tr_lbl="train-labels-idx1-ubyte"
+
+  te_img="t10k-images-idx3-ubyte"
+  te_lbl="t10k-labels-idx1-ubyte"
+
+const 
+  tInput* = 0 # images 784 image 0..1
+  tOutput* = 1 # labels array[10] w/ 1 in position
+
+type
+
+  Training* = array[2,vvec] # [imgs, labels]
+  file_types = enum tLabel = 2049, tImage = 2051
+
+
+  MNISTfile=object
+    stm : Stream
+    offset : int32
+    magic, nItems, rows, cols, imgSize : int32
+    imgBuff : seq[uint8]
+    ok : bool
+
+  MNIST* = object
+    tr_img, tr_lbl, te_img, te_lbl : MNISTfile
+
+## MNISTfile
+
+proc readi32(stm:Stream) : int32 = 
+  assert stm.readData(result.addr, int32.sizeof) == int32.sizeof
+  swapEndian32(result.addr, result.addr)
+
+proc mnist_img*(strm:Stream) : MNISTfile =
+  result.stm = strm
+  assert result.stm.isNil == false
+  result.magic = result.stm.readi32
+  assert result.magic == tImage.int32
+  result.n_items = result.stm.readi32
+  result.offset = 4 + 4 + 4 + 4
+  result.cols = result.stm.readi32
+  result.rows = result.stm.readi32
+  assert result.cols!=0  and result.rows!=0
+  result.imgSize = result.cols * result.rows
+  result.imgBuff = newSeq[uint8](result.imgSize)
+  result.ok=true
+
+proc mnist_lbl*(strm:Stream) : MNISTfile =
+  result.stm = strm
+  assert result.stm.isNil == false
+  result.magic = result.stm.readi32
+  assert result.magic == tLabel.int32
+  result.n_items = result.stm.readi32
+  assert result.n_items != 0
+  result.offset = 4 + 4
+  result.ok=true
+
+proc mnist_img*(name:string) : MNISTfile =
+  result.stm = newFileStream(name, fmRead)
+  assert result.stm.isNil == false
+  result.magic = result.stm.readi32
+  assert result.magic == tImage.int32
+  result.n_items = result.stm.readi32
+  result.offset = 4 + 4 + 4 + 4
+  result.cols = result.stm.readi32
+  result.rows = result.stm.readi32
+  assert result.cols!=0  and result.rows!=0
+  result.imgSize = result.cols * result.rows
+  result.imgBuff = newSeq[uint8](result.imgSize)
+  result.ok=true
+
+proc mnist_lbl*(name:string) : MNISTfile =
+  result.stm = newFileStream(name, fmRead)
+  assert result.stm.isNil == false
+  result.magic = result.stm.readi32
+  assert result.magic == tLabel.int32
+  result.n_items = result.stm.readi32
+  assert result.n_items != 0
+  result.offset = 4 + 4
+  result.ok=true
+
+proc get_byte(m : MNISTfile, item : int) : uint8 =
+  assert m.magic == tLabel.int32
+  m.stm.setPosition m.offset + item
+  result = m.stm.peekUint8
+
+proc read_image(m : var MNISTfile, item : int) =
+  assert m.magic == tImage.int32
+  m.stm.setPosition m.offset + item * m.imgSize
+  assert m.stm.readData(m.imgBuff[0].addr,  m.imgSize) == m.imgSize
+
+proc to_input_seq(m : MNISTfile) : seq[real] =
+  assert m.magic == tImage.int32
+  result = newSeq[real](m.imgSize)
+  for i, b in m.imgBuff.pairs:
+    result[i] = b.real/255 # if b > 127: 1 else: 0
+
+proc to_input_seq(m : var MNISTfile, i : int) : seq[real] =
+  m.read_image(i)
+  m.to_input_seq()
+
+proc get_label_seq(m : MNISTfile, i : int) : seq[real] =
+  result = newSeq[real](10)
+  result[ m.get_byte(i) ] = 1
+
+## MNIST
+
+proc `=destroy`(m: var MNIST)=
+  if not m.tr_img.stm.isNil: m.tr_img.stm.close
+  if not m.tr_lbl.stm.isNil: m.tr_lbl.stm.close
+  if not m.te_img.stm.isNil: m.te_img.stm.close
+  if not m.te_lbl.stm.isNil: m.te_lbl.stm.close
+
+proc newMNIST_zip* : MNIST =
+  var z : ZipArchive
+  
+  if z.open(zip_file):
+    result.tr_img = mnist_img(z.getStream(tr_img))
+    result.tr_lbl = mnist_lbl(z.getStream(tr_lbl))
+    result.te_img = mnist_img(z.getStream(te_img))
+    result.te_lbl = mnist_lbl(z.getStream(te_lbl))
+  z.close
+  
+proc newMNIST_file* : MNIST =
+  result.tr_img = mnist_img(path & tr_img)
+  result.tr_lbl = mnist_lbl(path & tr_lbl)
+  result.te_img = mnist_img(path & te_img)
+  result.te_lbl = mnist_lbl(path & te_lbl)
+  
+proc training_images*(m: var MNIST) : vvec =
+  for i in 0..<m.tr_img.nItems:  result.add(m.tr_img.to_input_seq(i))
+
+proc training_labels*(m: var MNIST) : vvec =
+  for i in 0..<m.tr_lbl.nItems:  result.add(m.tr_lbl.get_label_seq(i))
+
+proc test_images*(m: var MNIST) : vvec =
+  for i in 0..<m.te_img.nItems:  result.add(m.te_img.to_input_seq(i))
+
+proc test_labels*(m: var MNIST) : vvec =
+  for i in 0..<m.te_lbl.nItems:  result.add(m.te_lbl.get_label_seq(i))
+
+proc training_data*(m : var MNIST) : Training = [m.training_images(), m.training_labels()]
+proc test_data*(m : var MNIST) : Training = [m.test_images(), m.test_labels()]
+
+proc shuffle_training*(train : var Training) =
+  let tr_len = train[0].len
+  var 
+    shseq=toSeq(0..<tr_len)
+    sseq = shseq
+  shseq.shuffle
+
+  for (f,t) in zip(sseq, shseq):
+    swap(train[0][f], train[0][f])
+    swap(train[1][f], train[1][f])
---
+++ mnist/nn.nim
@@ -0,0 +1,125 @@
+# nn.nim perceptron mnist 
+# nim c -r -d:release --boundChecks:off --rangeChecks:off  nn
+
+import mnist, mat, random, sequtils, times, math, streams
+
+## NN
+
+type 
+  NN* = object
+    nlayers* : int
+    w*: vmat
+    b*: vvec
+    deltaW* : vmat
+    deltaB* : vvec
+
+proc newNN*(sizes:openarray[int]) : NN =
+  result.nlayers = sizes.len
+
+  randomize()
+  for y in sizes[1..^1]: result.b.add(rand_vec(y))
+  for (x,y) in zip(sizes[0..^1], sizes[1..^1]): result.w.add(rand_mat(y,x))
+
+  result.deltaW = result.w.from_shape # deltas=0
+  result.deltaB = result.b.from_shape
+
+proc evaluate*(nn:NN, input:vec):vec=
+  result = input
+
+  for (b,w) in zip(nn.b, nn.w):
+    result = w.wab(result, b).sigmoid
+
+proc test_factor*(nn:NN, test_data:Training):real=
+  for (input, output) in zip(test_data[tInput], test_data[tOutput]):
+    let a = nn.evaluate(input)
+    if a.maxIndex == output.find(1.0): 
+      result += 1
+
+  result / test_data[tInput].len.real
+
+ 
+proc cost_derivative(a,b:vec):vec = a - b
+
+proc backpropagation( nn : NN, input_data, test_data : vec) : (vmat, vvec) =
+  var
+    activation = input_data
+    activations, zs : mat
+  
+  # feed-fwd
+  activations.add(input_data)
+
+  for (b,w) in zip(nn.b, nn.w):
+    var z = w.wab(activation,b) # w*activation+b
+
+    zs.add(z)
+    activation = z.sigmoid
+    activations.add(activation)
+
+  # backward pass
+  var 
+    deltaW = nn.w.from_shape
+    deltaB = nn.b.from_shape
+    delta : vec = cost_derivative(activations[^1], test_data) * (zs[^1].sigmoid_prime)
+
+  deltaB[^1] = delta
+  deltaW[^1] = delta .** activations[^2]
+
+  for l in 2..<nn.nlayers: 
+    var sp = zs[^l].sigmoid_prime
+    delta = (nn.w[^(l-1)].transpose .* delta) * sp
+
+    deltaB[^l] = delta
+    deltaW[^l] = delta .** activations[^(l+1)]
+
+  (deltaW, deltaB)
+
+# 1 epoch learning
+proc learn*(nn:var NN, training_data: Training, eta : real) =
+  for (input, output) in zip(training_data[tInput], training_data[tOutput]):
+
+      let (deltaW, deltaB) = nn.backpropagation(input, output)
+
+      nn.w -= deltaW * eta
+      nn.b -= deltaB * eta
+
+proc SGD(nn : var NN, training_data, test_data : var Training, nepochs : int = 10, eta : real = 4.0) : real =
+
+  for epoch in 0..<nepochs:
+    let t = now()
+    write stdout, "epoch ", epoch, ": "; stdout.flushFile
+    
+    training_data.shuffle_training
+
+    nn.learn(training_data, eta)
+    
+    echo " learn factor:", (100.0 * nn.test_factor(test_data)).floor, "%, lap:",(now()-t).inSeconds,"\""
+
+  (nn.test_factor(test_data) * 100).floor
+
+proc save*(nn:NN, file_name : string)= # nim source can be used as training starting point
+  var s = newFileStream(file_name, fmWrite)
+  write s, "let weight_learnt* =", nn.w, "\n"
+  write s, "let biass_learnt* =", nn.b, "\n"
+  s.close
+
+##################
+when isMainModule:
+
+  proc test_nn* =
+    var mnist = newMNIST_file()
+
+    echo "loading training data..."
+
+    var 
+      training_data = mnist.training_data()
+      test_data = mnist.test_data()
+
+    if test_data[tInput].len==0: raise Exception.newException("empty test data, end") 
+
+    var nn = newNN [784, 30, 10]
+
+    echo "mnist training..."
+    echo "done, learn factor:", nn.SGD(training_data, test_data, nepochs=6, eta=0.4), "%"
+    nn.save("mnist/mnist_learnt.nim")
+
+  test_nn()
