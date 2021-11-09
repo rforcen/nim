cd2e81cbc8e5ea7c0aff334fe7ef194761a24857
roberto forcen <robertoforcen@gmail.com>
2021-11-09 10:20:26 +0100

    Add files via upload

--- mnist/nn.nim
+++ mnist/nn.nim
@@ -1,5 +1,5 @@
 # nn.nim perceptron mnist 
-# nim c -r -d:release --boundChecks:off --rangeChecks:off  nn
+# nim c -r -d:release -d:danger nn
 
 import mnist, mat, random, sequtils, times, math, streams
 
@@ -28,6 +28,14 @@ proc evaluate*(nn:NN, input:vec):vec=
 
   for (b,w) in zip(nn.b, nn.w):
     result = w.wab(result, b).sigmoid
+
+# digit 0..9 closer to input image
+proc eval_digit*(nn:NN, input:vec):int=
+  var r = input
+
+  for (b,w) in zip(nn.b, nn.w):
+    r = w.wab(r, b).sigmoid
+  r.maxIndex
 
 proc test_factor*(nn:NN, test_data:Training):real=
   for (input, output) in zip(test_data[tInput], test_data[tOutput]):
@@ -88,7 +96,7 @@ proc SGD(nn : var NN, training_data, test_data : var Training, nepochs : int = 1
     let t = now()
     write stdout, "epoch ", epoch, ": "; stdout.flushFile
     
-    training_data.shuffle_training
+    # training_data.shuffle_training # not really required
 
     nn.learn(training_data, eta)
     
@@ -114,9 +122,15 @@ when isMainModule:
       training_data = mnist.training_data()
       test_data = mnist.test_data()
 
-    if test_data[tInput].len==0: raise Exception.newException("empty test data, end") 
+    let 
+      img_sz = training_data[tInput][0].len
+      n_mid_layers = 30
+      n_digits = 10
 
-    var nn = newNN [784, 30, 10]
+    if test_data[tInput].len==0: 
+      raise Exception.newException("empty test data, end") 
+
+    var nn = newNN [img_sz, n_mid_layers, n_digits]
 
     echo "mnist training..."
     echo "done, learn factor:", nn.SGD(training_data, test_data, nepochs=6, eta=0.3), "%"
